f0d9ae9025da91a32fcf36d62c60865dd3bbfb11
JM008345 <hui.yang1@baozun.com>
2021-06-25 17:56:34 +0800

    版本1.0.0.0

--- server/main/db/config.js
+++ server/main/db/config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: HarlieYang
  * @Date: 2021-06-23 10:01:03
- * @LastEditTime: 2021-06-24 11:34:36
+ * @LastEditTime: 2021-06-25 17:52:31
  * @LastEditors: Please set LastEditors
  * @Description: 数据库配置文件
  * @FilePath: /HarlieBlog/server/main/db/config.js
@@ -12,7 +12,7 @@ var config = {
     database: 'blog',
     username: 'root',
     port: 3306,
-    password: '!Wzh18916152877',
+    password: '',
     host: 'rm-8vbh6cf3mfulpa660qo.mysql.zhangbei.rds.aliyuncs.com'
 };
 
